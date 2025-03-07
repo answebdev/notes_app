d980de3ee569f789f6f8aad92bfabbcc364ef8e6
Adolf Schmuck <britvaica@netscape.net>
2025-03-06 18:56:31 -0800

    add note component

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,9 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const NotesScreen = () => {
+  const [notes, setNotes] = useState([
+    { id: '1', text: 'Note One' },
+    { id: '2', text: 'Note Two' },
+    { id: '3', text: 'Note Three' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Notes</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={notes}
+        // renderItem: This is where we return the JSX that we want to display:
+        renderItem={({ item }) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.text}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -14,8 +30,17 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#ffffff',
   },
+  noteItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#f5f5f5',
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18,
+  },
 });
 
 export default NotesScreen;
-
-// STOPPED AT 34:20
